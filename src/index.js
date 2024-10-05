dee1b7c9105a762078bd02425540a755e10a1360
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-10-06 01:09:08 +0530

    Finished adding the 'Default Project' project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import  {Project, ProjectList} from "./classModule"
 import "./styles.css"
 import {formButtonFunctionality, displayProjectList, projectListObject, deleteButtonFunctionality, displayTasksFromProjectList} from "./domManipulationModule";
-import { initializeProjectsFromSessionStorage } from "./webStorageAPI";
-
-// const project_1 = new Project ('Default Project');
-// projectListObject.addProjectToList = project_1;
-// project_1.addTask('title#1', 'Desc', 'DueDate', 'Priority');
-// let projectArr = project_1.getToDoList();
-// console.log(projectArr.length)
-// console.log(projectArr[0].values);
-// projectArr[0].editTask('title#3', 'Asc', '', 'Priority123', 'new note');
-// console.log(`After: ${projectArr[0].values}`);
-
-// for (let i in projectListObject.projectListArray) {
-//     console.log(projectListObject.projectListArray[i]);
-// }
+import { initializeProjectsFromSessionStorage, populateStorage, fetchStorage } from "./webStorageAPI";
 
 initializeProjectsFromSessionStorage();
+
+// implement 'Default Project' to session storage and projectListObject
+if (!fetchStorage().length) {
+    projectListObject.addProjectToList='Default Project';
+    populateStorage(projectListObject.projectListArray);
+}
+
 formButtonFunctionality();
 displayProjectList();
 deleteButtonFunctionality();
